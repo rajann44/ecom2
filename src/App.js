16b13ee70b591a5496988f9c7c05e8db75b3cba4
Rajan Chaudhary <rajann44@gmail.com>
2023-06-11 23:56:10 +0200

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      {/* <Header></Header> */}
+      <Routes>
+        <Route path="/" element={<div>Hello</div>}></Route>
+      </Routes>
+      {/* <Footer></Footer> */}
+    </div>
+  );
 }
 
 export default App;
